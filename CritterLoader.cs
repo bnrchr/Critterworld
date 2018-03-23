c50d13f73350e551d1361c90880290cb342a0d45
bnrchr <36008327+bnrchr@users.noreply.github.com>
2018-03-23 15:59:43 +0000

    Add files via upload
    Created new project within the solution. Started first critter class. Started loader class.

---
+++ CritterLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using CritterBrains;
+
+namespace _100464667
+{
+    class CritterLoader : ICritterFactory
+    {
+        public interface ICritterFactory
+        {
+            IEnumerable<CritterBrains.CritterBrain> GetCritterBrains();
+        }
+    }
+}
